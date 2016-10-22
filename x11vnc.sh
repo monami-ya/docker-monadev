5645a7859bcb21f69c49763fbdcd08bf6cc0160f
Masaki Muranaka <monaka@monami-ya.com>
2016-10-22 09:30:16 +0000

    x11vnc: set -xkb and disable clipboard.
    They are for Eclipse.

--- x11vnc.sh
+++ x11vnc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/bin/x11vnc -v -forever -nopw -display :0 -auth /home/monaka/.Xauthority
+exec /usr/bin/x11vnc -v -forever -xkb -noclipboard -nosetclipboard -nopw -display :0 -auth /home/monaka/.Xauthority
